2bb9b02d06542a5c66fe4e59986255d45d01bbcf
Kanade <178143794+fujigawayuki@users.noreply.github.com>
2024-08-25 10:35:20 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone -b thirteen https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b Swordx/Ksu-Staging https://github.com/ardia-kun/kernel_xiaomi_fog kernel/xiaomi/fog
+git clone --depth=1 -b Swordx-ksu-13 https://github.com/ardia-kun/kernel_xiaomi_fog kernel/xiaomi/fog
 
 # Clone 'PixelExperience' hardware/xiaomi
 git clone -b thirteen https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
