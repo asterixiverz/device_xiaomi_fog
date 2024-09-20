c0f2e2b96a60e1b98530ee2994f73f4f45543fd5
develux <103586125+develux44@users.noreply.github.com>
2024-09-20 21:44:09 +0500

    fog: rootdir: Fix high cpy freq

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -78,19 +78,19 @@ echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
 echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
-echo 1516800 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
-echo 691200 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1804800 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
+echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rtg_boost_freq
 
 # configure governor settings for big cluster
 echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us
 echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us
-echo 1344000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
-echo 1056000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo 2208000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
+echo 300000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rtg_boost_freq
 
-echo "0:1190000" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+echo "0:1190000 4:1344000" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
 echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
 # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
