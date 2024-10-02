9695a974acf25be4271938f7ad913c6fe9315953
Iverz <asterixiverz@gmail.com>
2024-10-02 19:24:07 +0000

    fog: vendorsetup: remove device stuff command
    now moved to local_manifest.
    
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,6 @@
 # Remove Pixel kernel header
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
-# Clone vendor tree
-git clone -b thirteen https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
-
-# Clone kernel tree
-git clone --depth=1 -b 4.19 https://github.com/asterixiverz/kernel_xiaomi_fog-msm4.19 kernel/xiaomi/fog
-
-# Clone hardware/xiaomi from @asterixiverz (Forking from PixelOS-AOSP)
-git clone -b thirteen-aosp https://github.com/asterixiverz/hardware_xiaomi hardware/xiaomi
-
-# Clone vendor MIUI Camera
-git clone -b uvite-sm6225 https://gitlab.com/soverzion-hq/proprietary_vendor_xiaomi_camera vendor/xiaomi/camera
-
-# Clang Missing Fixups
-rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
-git clone https://gitlab.com/tejas101k/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
-
 # Kernel fixups
 cd kernel/xiaomi/fog
 rm -rf Android.bp
